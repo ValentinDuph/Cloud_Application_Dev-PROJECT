9098a64ed22c036f4fd4f6af62c662fa1316e0a3
akiraquenot <akira.quenot@gmail.com>
2017-11-30 14:37:22 +0100

    Testing samples of code

--- node_app/server.js
+++ node_app/server.js
@@ -1,36 +1,45 @@
+/// ALL REQUIRED PACKAGES
 var http = require('http');
-var url = require("url");
+var url = require('url');
 var querystring = require('querystring');
+var fct = require('functions'); // Fait appel à functions_lib.js (dans le dossier functions)
+var express = require('express');
+var path = require('path');
+var mongodb = require('mongodb');
 
-var server = http.createServer(function(req, res) {
-  var get_url = url.parse(req.url);
-  var page = get_url.pathname;
-  var params = querystring.parse(get_url.query);
+// CONSTANTE VARIABLES
+const app = express();
+const mongoClient = mongodb.MongoClient;
+const url_db = "mongodb://localhost:27017/Airline";
 
-  console.log(get_url);
-  console.log(page);
-  console.log(params);
+// Store all HTML files in view folder.
+app.use(express.static('www'));
 
-  res.writeHead(200, {"Content-Type": "text/plain"});
 
-  // get params :
-  // url.parse(req.url).query
+app.get('/findOne', function(req,res) {
+  mongoClient.connect(url_db, function(err, db) {
+    if (err) throw err;
+    db.collection("On_Time_On_Time_Performance_2016_1").findOne({}, function(err, result) {
+      if (err) throw err;
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(result));
+      db.close();
+    });
+  });
+})
 
-  if (page == "/contact") {
-    //Send contact page
-  }
-  else if (page == "/private") {
-    res.write('This is a protected area')
-  }
-  else {
-    //Send index page
-  }
+app.get('/test', function (req, res) {
+  res.sendFile( __dirname + "/www/index0.html" );
+})
 
-  res.end();
-});
+var server = app.listen(8080, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port);
+  fct.direBonjour();
+})
 
 server.on('close', function() { // On écoute l'évènement close
   console.log('Bye bye !');
 })
-
-server.listen(8000);
