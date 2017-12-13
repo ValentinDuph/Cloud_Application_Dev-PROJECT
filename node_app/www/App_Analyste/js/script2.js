9a6709a92a953e5e154bd569ad51689f3b1b947d
akiraquenot <akira.quenot@gmail.com>
2017-12-13 11:22:34 +0100

    Adding a progress bar in Analyst Interface

--- node_app/www/App_Analyste/js/script2.js
+++ node_app/www/App_Analyste/js/script2.js
@@ -6,6 +6,7 @@ function onload() {
 function getAirportList() {
   $.getJSON('db_data?q=airports', function(data) {
       var list = document.getElementById("originAiports");
+      document.getElementById("loader").innerHTML = "";
       for (item of data) {
         var option = document.createElement("option");
         option.text = item._id;
@@ -19,12 +20,29 @@ function getAirportList() {
 
 function displayInfo() {
   var selectedAiport = document.getElementById("originAiports").value;
+  move(0,20);
   diplayDelayAvg(selectedAiport);
   displayDelayBar(selectedAiport);
 }
 
+function move(from, to) {
+    var elem = document.getElementById("result-progress");
+    var width = from;
+    var id = setInterval(frame, 10);
+
+    function frame() {
+        if (width >= to) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
 function diplayDelayAvg(selectedAiport) {
   $.getJSON('db_data?q=dep_delay_avg&airport='+selectedAiport, function(data) {
+    move(20,40);
     document.getElementById("dep-delay-container").innerHTML =
       '<div class="c100 p' + parseInt(parseFloat(data[0].avg_Delay)*100) + ' dark big orange" style=" display: table; margin: 0 auto;">'
       + '<span>' + parseInt(parseFloat(data[0].avg_Delay)*100) + '%</span>'
@@ -35,6 +53,7 @@ function diplayDelayAvg(selectedAiport) {
       + '</div>';
   });
   $.getJSON('db_data?q=arr_delay_avg&airport='+selectedAiport, function(data) {
+    move(40,60);
     document.getElementById("arr-delay-container").innerHTML =
       '<div class="c100 p' + parseInt(parseFloat(data[0].avg_Delay)*100) + ' dark big orange" style=" display: table; margin: 0 auto;">'
       + '<span>' + parseInt(parseFloat(data[0].avg_Delay)*100) + '%</span>'
@@ -48,8 +67,10 @@ function diplayDelayAvg(selectedAiport) {
 
 function displayDelayBar(selectedAiport) {
   $.getJSON('db_data?q=dep_delay_avg&airport='+selectedAiport, function(json_data) {
+    move(60,80);
     var depDelayTime = parseFloat(json_data[0].avg_DelayTime);
     $.getJSON('db_data?q=arr_delay_avg&airport='+selectedAiport, function(json_data2) {
+      move(80,100);
       var arrDelayTime = parseFloat(json_data2[0].avg_DelayTime);
       var data = {
         labels: ["Departures", "Arrivals"],
