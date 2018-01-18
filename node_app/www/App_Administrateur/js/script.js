a632f70962f26c9a247aa5d8adeae5a1f3da70f2
Akira Quenot <akiraquenot@MacBook-Pro-de-Akira.local>
2018-01-18 17:20:28 +0100

    "Nothing to show" --> ADM

--- node_app/www/App_Administrateur/js/script.js
+++ node_app/www/App_Administrateur/js/script.js
@@ -5,6 +5,7 @@ function onload() {
 function getLog() {
   $.getJSON('db_data?q=getLog', function(data) {
       var user_console = document.getElementById("user_console");
+      user_console.innerHTML = ""
       for (item of data) user_console.innerHTML += item.date + ' : ' + ' [' + item.type + '] ' + item.query + " = " + item.time + ' ms <br>';
   });
 }
